1db92596667b110ce82637572d94bbebe5189525
ajjeon@marengo <jeonahjung@gmail.com>
2020-04-30 13:45:19 +0800

    updated lesson4

--- lesson4.R
+++ lesson4.R
@@ -14,45 +14,54 @@ library(DESeq2)
 ### Alternatively, download the genewise counts. These are essentially the same.
 
 dataset <- read.table("data/GSE60450_Lactation-GenewiseCounts.txt", header=TRUE)
+
 colnames(dataset)[3:14] <- sapply(colnames(dataset)[-1][-1],
                                   function(x) strsplit(x, "_")[[1]][1])
 
 counts <- dataset[,-1][,-1]
+
 rownames(counts) <- as.character(dataset$EntrezGeneID)
 
 metadata <- read.csv("data/metadata.csv", row.names=1)
+
+
+## DEseq starts here
 
 dds <- DESeqDataSetFromMatrix(countData = counts,
                        colData = metadata,
                        design = ~ devstage + celltype)
 
-keep <- rowSums(counts(dds)) >=10
+keep <- rowSums(counts(dds)) >= 10
+
 dds <- dds[keep,]
 
 dds <- DESeq(dds)
 
 res_celltype <- results(dds)
-res_dev <- results(dds, contrast = c("devstage", "virgin", "18.5dP"))
 
-resSig <- subset(res, res$padj < 0.1 )
+res_dev <- results(dds, contrast = c("devstage","18.5dP", "virgin"))
+
+resSig <- subset(res_dev, res$padj < 0.1 )
 
 head(resSig[ order( -resSig$log2FoldChange ), ])
 
 plotMA(res_dev)
+plotMA(res_celltype)
 
-
+#plot(res_dev$log2FoldChange, res_dev$pvalue)
 
 ### Working with normalized counts
 rld <- rlog(dds) # normalization
 plotPCA(rld, intgroup=c("devstage","celltype"))
 
 library( "genefilter" )
+
 topVarGenes <- head( order( rowVars( assay(rld) ), decreasing=TRUE ), 35 )
 
-library(gplots)
+library(gplots) #heatmap.2
 library(RColorBrewer)
-library("heatmap.plus")
-library("biomaRt")
+library(heatmap.plus) # heatmap.plus function 
+library(biomaRt)
 
 mart <- useMart(biomart = "ensembl",
                 dataset = "mmusculus_gene_ensembl")
